b9d9e5a73e993331011bbab44ff48eb64ca3cc1b
Raxhet <50268115+Raxhet@users.noreply.github.com>
2023-01-23 23:07:54 +0800

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Илья Меркуленко on 23.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
