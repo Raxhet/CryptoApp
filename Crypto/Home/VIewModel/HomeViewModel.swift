788d831c98624f8bf9fe00c499df743688ad8e70
Raxhet <50268115+Raxhet@users.noreply.github.com>
2023-01-31 00:04:36 +0800

    Now SearchBarView can filtering coins in home screen

--- Crypto/Home/VIewModel/HomeViewModel.swift
+++ Crypto/Home/VIewModel/HomeViewModel.swift
@@ -27,10 +27,29 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
-        dataService.$allCoins
-            .sink { [weak self] (returnedCoins) in
+        
+        //udpating allCoins variable
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
+            .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        
+        return coins.filter { coin in
+            return coin.name.lowercased().contains(lowercasedText) ||
+                   coin.symbol.lowercased().contains(lowercasedText) ||
+                   coin.id.lowercased().contains(lowercasedText)
+        }
     }
 }
